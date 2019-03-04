f758d4e1d693a5bc79ec4c73fc7491ae7dfb09be
vsa <vsa@vsa-VirtualBox>
2019-03-04 14:53:10 +0100

    git repeat added al files

---
+++ VSA_2_1task/src/vsa_2_1task/VSA_2_1task.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vsa_2_1task;
+
+/**
+ *
+ * @author vsa
+ */
+public class VSA_2_1task {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
